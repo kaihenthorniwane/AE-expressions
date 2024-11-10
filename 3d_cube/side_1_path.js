2f4b2644f8dbf92d1cbbdf5b41001a04f441ae71
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-11-10 11:44:56 -0800

    rotations

--- 3d_cube/side_1_path.js
+++ 3d_cube/side_1_path.js
@@ -1,8 +1,10 @@
 // this expression calculates the path of a side for a 3d cube
 
 const radius = effect("Radius")("Slider");
-const verticalRotation = effect("Vertical Rotation")("Slider");
-const horizontalRotation = effect("Horizontal Rotation")("Slider");
+const verticalRotation = degreesToRadians(effect("Vertical Rotation")("Angle"));
+const horizontalRotation = degreesToRadians(
+  effect("Horizontal Rotation")("Angle")
+);
 
 const initialPathPointsXYZ = [
   [-radius, radius, radius],
@@ -10,3 +12,34 @@ const initialPathPointsXYZ = [
   [radius, -radius, radius],
   [-radius, -radius, radius],
 ];
+
+const rotatePathPointsAlongXAxisAKAVerticalRotation = initialPathPointsXYZ.map(
+  (point) => {
+    return [
+      point[0],
+      point[1] * Math.cos(verticalRotation) -
+        point[2] * Math.sin(verticalRotation),
+      point[1] * Math.sin(verticalRotation) +
+        point[2] * Math.cos(verticalRotation),
+    ];
+  }
+);
+
+const rotatePathPointsAlongYAxisAKAHorizontalRotation =
+  rotatePathPointsAlongXAxisAKAVerticalRotation.map((point) => {
+    return [
+      point[0] * Math.cos(horizontalRotation) +
+        point[2] * Math.sin(horizontalRotation),
+      point[1],
+      -point[0] * Math.sin(horizontalRotation) +
+        point[2] * Math.cos(horizontalRotation),
+    ];
+  });
+
+const flattenedToXY = rotatePathPointsAlongYAxisAKAHorizontalRotation.map(
+  (point) => {
+    return [point[0], point[1]];
+  }
+);
+
+flattenedToXY;
