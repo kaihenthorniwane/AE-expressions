9d9a2f1a7798c4b626da585ab52f77a7ad959e15
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-11-09 21:30:06 -0800

    add scale offset property

--- cd_thing/main_box_transform_scale.js
+++ cd_thing/main_box_transform_scale.js
@@ -1,1 +1,14 @@
-// this is an after effects expression on a shape group that undoes any scale applied to the parent shape layer
+// this gets applied to "Transform Offsetter" scale property
+
+const toCompPointToMeasure = [1, 1];
+const constantPointToMeasure = [0, 0];
+
+const constantPoint = toComp(constantPointToMeasure);
+const measuredPoint = toComp(toCompPointToMeasure);
+
+const offsetPoint = measuredPoint - constantPoint;
+
+const offsetScaleX = 1 / offsetPoint[0];
+const offsetScaleY = 1 / offsetPoint[1];
+
+[offsetScaleX * 100, offsetScaleY * 100];
